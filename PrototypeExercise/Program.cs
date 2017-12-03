d74dcbe736e40fcb76d8050057aa0134fa25ff36
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-12-03 13:11:51 -0800

    Add two different implementations for deep copy for the prototype exercise. dotnetstandard does not support binary serialization so I created an explicity deep copy interface call back.

---
+++ PrototypeExercise/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace PrototypeExercise
+{
+    public static class ExtensionMethods
+    {
+        // an extension method for all objects
+        public static T DeepCopy<T>(this T self)
+        {
+            // serialize using the binary serializer
+            var stream = new MemoryStream();
+            var formatter = new BinaryFormatter();
+            formatter.Serialize(stream, self);
+            stream.Seek(0, SeekOrigin.Begin);
+            object copy = formatter.Deserialize(stream);
+            stream.Close();
+            return (T)copy;
+        }
+    }
+
+    // all types have to have this attribute
+    [Serializable]
+    public class Point
+    {
+        public int X, Y;
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+
+    [Serializable]
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return this.DeepCopy<Line>();
+        }
+
+        public override string ToString()
+        {
+            return $"{Start.ToString()} -> {End.ToString()}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = new Line { Start = new Point { X = 10, Y = 20 }, End = new Point { X = 800, Y = 600 } };
+            var line2 = line.DeepCopy();
+
+            line.Start = new Point { X = 24, Y = 32 };
+            line.End = new Point { X = 24, Y = 32 };
+
+            Console.WriteLine(line);
+            Console.WriteLine(line2);
+        }
+    }
+}
---
+++ PrototypeExerciseWithExplicitInterface/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+namespace PrototypeExerciseWithExplicitInterface
+{
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+
+    public class Point : IPrototype<Point>
+    {
+        public int X, Y;
+
+        public Point DeepCopy()
+        {
+            return new Point { X = this.X, Y = this.Y };
+        }
+
+        public override string ToString()
+        {
+            return $"({X},{Y})";
+        }
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line DeepCopy()
+        {
+            return new Line { Start = this.Start.DeepCopy(), End = this.End.DeepCopy() };
+        }
+
+        public override string ToString()
+        {
+            return $"{Start.ToString()} -> {End.ToString()}";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = new Line { Start = new Point { X = 10, Y = 20 }, End = new Point { X = 800, Y = 600 } };
+            var line2 = line.DeepCopy();
+
+            line.Start = new Point { X = 24, Y = 32 };
+            line.End = new Point { X = 24, Y = 32 };
+
+            Console.WriteLine(line);
+            Console.WriteLine(line2);
+        }
+    }
+}
