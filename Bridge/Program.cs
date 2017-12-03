91aa3e42f572f594f255acc840c1ea389ab993c9
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-12-03 15:01:06 -0800

    Simple bridge pattern, inject different renderers into a circle draw code

---
+++ Bridge/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Bridge
+{ 
+    public interface IRenderer
+    {
+        void RenderCircle(double radius);
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public void RenderCircle(double radius)
+        {
+            Console.WriteLine($"Rendering a vector circle with radius {radius}");
+        }
+    }
+
+    public class PixelRenderer : IRenderer
+    {
+        public void RenderCircle(double radius)
+        {
+            Console.WriteLine($"Rendering a pixel circle with radius {radius}");
+        }
+    }
+
+    public abstract class Shape
+    {
+        protected IRenderer renderer;
+        protected Shape(IRenderer renderer)
+        {
+            if (renderer == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            this.renderer = renderer;
+        }
+
+        public abstract void Draw();
+        public abstract void Resize(float factor);
+    }
+
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(IRenderer renderer, double radius) : base(renderer)
+        {
+            this.radius = radius;
+        }
+
+        public override void Draw()
+        {
+            renderer.RenderCircle(radius);
+        }
+
+        public override void Resize(float factor)
+        {
+            radius *= factor;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var renderer = new VectorRenderer();
+            var otherRenderer = new PixelRenderer();
+            var circle = new Circle(renderer, 10.0);
+            circle.Draw();
+            var circle2 = new Circle(otherRenderer, 20.0);
+            circle2.Draw();
+        }
+    }
+}
