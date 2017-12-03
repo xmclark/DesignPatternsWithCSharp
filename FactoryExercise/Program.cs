053edce8eeee129ec0dbc0ff221f76d8645ad003
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-12-03 11:43:58 -0800

    Add the solution to the factory exercise

---
+++ FactoryExercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace FactoryExercise
+{
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+    }
+
+    public class PersonFactory
+    {
+        private int id = 0;
+        public Person CreatePerson(string name)
+        {
+            return new Person { Id = id++, Name = name };
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new PersonFactory();
+
+            var john = factory.CreatePerson("John");
+            var rachel = factory.CreatePerson("Rachel");
+
+            var anotherFactory = new PersonFactory();
+            var jose = anotherFactory.CreatePerson("Jose");
+
+            Console.WriteLine(john);
+            Console.WriteLine(rachel);
+
+            Console.WriteLine(jose);
+
+
+        }
+    }
+}
