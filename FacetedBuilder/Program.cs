79b4b8fcb32cda61d4a09bb0384b758efca01500
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-12-03 08:58:20 -0800

    Add facade builder with sub builders for a person class and clever use of implicit operator - woot!

---
+++ FacetedBuilder/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace FacetedBuilder
+{
+    public class Person
+    {
+        public string StreetAddress, PostCode, City;
+
+        public string CompanyName, Position;
+
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(PostCode)}: {PostCode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}.";
+        }
+    }
+
+    public class PersonBuilder // facade
+    {
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+        public PersonAddressBuilder Lives => new PersonAddressBuilder(person);
+
+        public static implicit operator Person(PersonBuilder pb)
+        {
+            return pb.person;
+        }
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    {
+        public PersonAddressBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonAddressBuilder At(string address)
+        {
+            person.StreetAddress = address;
+            return this;
+        }
+
+        public PersonAddressBuilder InPostCode(string postCode)
+        {
+            person.PostCode = postCode;
+            return this;
+        }
+
+        public PersonAddressBuilder InCity(string city)
+        {
+            person.City = city;
+            return this;
+        }
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earnings(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            Person person = pb
+                .Works.At("Joe's Coffee Shop").AsA("Barista").Earnings(25000)
+                .Lives.At("123 Main Street").InPostCode("99999").InCity("Cooltown");
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
