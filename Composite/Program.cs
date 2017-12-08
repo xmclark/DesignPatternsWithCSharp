eaa66aba03dfc03b3de75e12b323f416cd62ddab
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2017-12-07 20:52:59 -0800

    Add comopsite pattern example

---
+++ Composite/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+
+    public class GraphicObject
+    {
+        public virtual string Name { get; set; } = "Group";
+        public string Color;
+
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+
+        public void Print(StringBuilder sb, int depth)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle";
+    }
+
+    public class Square : GraphicObject
+    {
+        public override string Name => "Square";
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
